8ee9d10a944cf37476c279079d59cf1bb2805ec0
magoudi <youssefmagedan@gmail.com>
2024-04-24 14:01:59 -0700

    f

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - krsgnusbfgoiajsgbksng
+ * @array: dfknbfbnsfjfvsd
+ * @size: sfjkbisfbagbnsnafgk
+ * Return: sfjbgkasfbfnkhsgrf
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (i = 1; i < size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+sdfngjsndfijgbnjgfkhfnibndghgjsfkbjdf boisn
