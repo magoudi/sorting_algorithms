645b7af938c7b3848e1047df4a7e789cad5855bf
magoudi <youssefmagedan@gmail.com>
2024-04-24 16:07:46 -0700

    f

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * selection_sort - wjgnvfgviugvkjnsbfnghbvsfgsdh
+ * @array: sfjnvasbfgnasghdbvnm
+ * @size: asfnbgasghbjnakhfgb
+ * Return:gsnfgjvndkfjbgnkjdfkng
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, index;
+	int min;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		min = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (min > array[j])
+			{
+				min = array[j];
+				index = j;
+			}
+		}
+		if (array[i] == min)
+			return;
+		array[index] = array[i];
+		array[i] = min;
+		print_array(array, size);
+	}
+}
