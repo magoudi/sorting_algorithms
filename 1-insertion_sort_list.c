3ffb3ba1fd2592f7dcddc8288c2540b2e4f12723
root <root@kirolos>
2024-04-30 23:49:52 +0300

    f

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,36 +1,47 @@
-#include "sort.h"
+include "sort.h"
 
 /**
- * insertion_sort_list - function that sorts a doubly linked list
- * of integers in ascending order using the Insertion sort algorithm
- * @list: Pointer to the head of the linked list
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * using the Insertion sort algorithm
+ * @list: pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-	return;
+	listint_t *node;
 
-	listint_t *current = (*list)->next;
-
-	while (current != NULL)
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
 	{
-	listint_t *inner = current->prev;
-	int key = current->n;
-
-	while (inner != NULL && inner->n > key)
-	{
-		inner->next->n = inner-n;
-		inner = inner->prev;
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
 	}
+}
 
-	if (inner == NULL)
-	{
-	(*list)->n = key;
-	}
+/**
+ * swap_node - swap a node for his previous one
+ * @node: node
+ * @list: node list
+ * Return: return a pointer to a node which was inserted it
+ */
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
 	else
-	{
-	inner->next->n = key;
-	}
-	current = current->next;
-	}
+		*list = current;
+	return (current);
 }
