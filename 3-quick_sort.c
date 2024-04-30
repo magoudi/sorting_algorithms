27e394f4ef78ae9782a90c81093924faa653e2c6
magoudi <youssefmagedan@gmail.com>
2024-04-30 08:08:44 -0700

    f

---
+++ 3-quick_sort.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * lomuto - skfhgahfbsajfbgfndkfbhd
+ * @array: jdfgonbakjsbrgsfjkbg
+ * @low: dfgrg
+ * @high: adsgarsg
+ * Return: werjfgnjbdg
+ */
+
+void lomuto(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i1 = low - 1;
+	int tmp;
+	int i;
+
+	if ((high - low) == 1)
+                return;
+	for (i = low; i < high; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			i1++;
+			if (array[i] == array[i1])
+				continue;
+			if (array[i] < array[i1])
+			{
+				tmp = array[i];
+				array[i] = array[i1];
+				array[i1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+	i1++;
+	if (array[high] != array[i1])
+	{
+		tmp = array[high];
+		array[high] = array[i1];
+		array[i1] = tmp;
+		print_array(array, size);
+	}
+	if (i1 > low)
+		lomuto(array, low, i1 - 1, size);
+	if (i1 < high)
+		lomuto(array, i1 + 1, high, size);
+}
+
+/**
+ * quick_sort - ljfbgfasbfhsbdjfgbhksdf
+ * @array: djfngkbbjgvbf.ksajhbknf
+ * @size: sfbkamsbfknkjfbjasdfbj
+ * Return: void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	lomuto(array, 0, size - 1, size);
+}
