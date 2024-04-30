6c8063b8f68659c6934e5a1ce55f8518ba39f733
root <root@kirolos>
2024-05-01 00:04:56 +0300

    f

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,40 @@
-include "sort.h"
+#include "sort.h"
+//#include <stdio.h>
 
 /**
  * insertion_sort_list - sorts a doubly linked list of integers in ascending order
  * using the Insertion sort algorithm
- * @list: pointer to the head of the doubly linked list
+ * @list: Pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	if (list == NULL || (*list)->next == NULL)
-		return;
-	node = (*list)->next;
-	while (node)
-	{
-		while ((node->prev) && (node->prev->n > node->n))
-		{
-			node = swap_node(node, list);
-			print_list(*list);
-		}
-		node = node->next;
-	}
-}
+    listint_t *current = (*list)->next;
 
-/**
- * swap_node - swap a node for his previous one
- * @node: node
- * @list: node list
- * Return: return a pointer to a node which was inserted it
- */
-listint_t *swap_node(listint_t *node, listint_t **list)
-{
-	listint_t *back = node->prev, *current = node;
+    while (current != NULL)
+    {
+        listint_t *inner = current->prev;
+        while (inner != NULL && inner->n > current->n)
+        {
+            if (inner->prev != NULL)
+                inner->prev->next = current;
+            if (current->next != NULL)
+                current->next->prev = inner;
 
-	back->next = current->next;
-	if (current->next)
-		current->next->prev = back;
-	current->next = back;
-	current->prev = back->prev;
-	back->prev = current;
-	if (current->prev)
-		current->prev->next = current;
-	else
-		*list = current;
-	return (current);
+            inner->next = current->next;
+            current->prev = inner->prev;
+            inner->prev = current;
+            current->next = inner;
+
+            if (current->prev == NULL)
+                *list = current;
+
+            print_list(*list);
+
+            inner = current->prev;
+        }
+        current = current->next;
+    }
 }
